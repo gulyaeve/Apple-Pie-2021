d787c6052731e7c6774bc97d87eb60deebd94fab
gulyaeve <gulyaev.e@icloud.com>
2021-06-07 13:36:22 +0300

    Initial Commit

---
+++ Apple Pie 2021/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Apple Pie 2021
+//
+//  Created by Евгений Гуляев on 07.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
