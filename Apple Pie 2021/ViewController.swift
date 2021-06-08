54c806c83e6a9e847973b2314d229a99710a5fff
gulyaeve <gulyaev.e@icloud.com>
2021-06-08 11:59:32 +0300

    Added game model

--- Apple Pie 2021/ViewController.swift
+++ Apple Pie 2021/ViewController.swift
@@ -9,11 +9,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - IB Outlets
+    @IBOutlet weak var treeImageView: UIImageView!
+    @IBOutlet var letterButtons: [UIButton]!
+    @IBOutlet weak var correctWordLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    // MARK: - Properties
+    var currentGame: Game!
+    let incorrectMovesAllowed = 7
+    var listOfWords = [
+        "Москва",
+        "Владимир",
+        "Тверь"
+    ]
+    var totalWins = 0
+    var totalLoses = 0
+    
+    // MARK: - Methods
+    func newRound() {
+        let newWord = listOfWords.removeFirst()
+        currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed)
+        updateUI()
+    }
+    
+    func updateUI() {
+        let movesRemaining = currentGame.incorrectMovesRemaining
+        let image = "Tree\(movesRemaining < 8 ? movesRemaining : 7)"
+        treeImageView.image = UIImage(named: image)
+        scoreLabel.text = "Выигрыши: \(totalWins), проигрыши: \(totalLoses)"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        newRound()
         // Do any additional setup after loading the view.
     }
 
-
+    // MARK: - IB Actions
+    @IBAction func letterButtonPressed(_ sender: UIButton) {
+        sender.isEnabled = false
+    }
 }
 
---
+++ Apple Pie 2021/game.swift
@@ -0,0 +1,11 @@
+//
+//  game.swift
+//  Apple Pie 2021
+//
+//  Created by Евгений Гуляев on 08.06.2021.
+//
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+}
